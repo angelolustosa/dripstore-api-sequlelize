e0de8e7c82be7545fab5bea1fc888f986e556576
angelolustosa <angelolustosa@gmail.com>
2023-12-13 00:38:12 -0300

    ajuste no db para subir

--- config/db.js
+++ config/db.js
@@ -3,18 +3,8 @@ import { Sequelize } from 'sequelize';
 import { DB_CONFIG } from './db/config/config.js';
 import 'dotenv/config' //https://www.npmjs.com/package/dotenv#how-do-i-use-dotenv-with-import
 
-
-
-
-
-// async function init() {
-// Conectar ao banco de dados principal para verificar se "dc" existe
-/* const sequelizeMaster = new Sequelize('postgres://seu_usuario:sua_senha@localhost:5432/postgres', {
-  dialect: 'postgres',
-}); */
-
 // Agora, você pode conectar diretamente ao banco de dados "dc"
-const sequelizePostgres = new Sequelize(process.env.URL_RENDER_POSTGRES || 'postgres://postgres:postgres@localhost:5432/postgres?sslmode=require', {
+const sequelizePostgres = new Sequelize(process.env.URL_RENDER_POSTGRES || 'postgres://postgres:postgres@localhost:5432/postgres', {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
@@ -42,44 +32,26 @@ try {
     }
 } catch (error) {
     console.error('Erro:', error);
-}/*  finally {
-      // Certifique-se de fechar as conexões
-      await sequelizeMaster.close();
-    } */
-//}
+}
 
-
-// sequelizePostgres.options.database = 'dc';  // Altera a configuração do banco de dados para "dc"
-// export const sequelize = new Sequelize(sequelizePostgres.options);
-
-export const sequelize = new Sequelize(
-    DB_CONFIG.DB, DB_CONFIG.USER, DB_CONFIG.PASSWORD,
+export const sequelize = new Sequelize(DB_CONFIG.DB, DB_CONFIG.USER, DB_CONFIG.PASSWORD,
     {
         host: DB_CONFIG.HOST,
         dialect: DB_CONFIG.dialect,
         dialectOptions: {
             ssl: {
-                //require: true,
+                require: true,
                 rejectUnauthorized: false, // Configuração para evitar o erro "SSL/TLS required"
             },
         },
-        port: 5432,
+        port: DB_CONFIG.PORT,
         pool: { ...DB_CONFIG.pool },
+        //logging: false
     }
 );
-
-console.log(`DB_CONFIG`, JSON.stringify(DB_CONFIG));
 
 sequelize.authenticate().then(() => {
     console.log('[INFO] Connection has been established successfully.');
 }).catch((error) => {
     console.error('[ERROR] Unable to connect to the database: ', error);
 });
-
-
-/* sequelize.sync({ force: true }).then(() => {
-    console.log('Tabelas sincronizadas com sucesso!');
-}).catch((error) => {
-    console.error('Erro ao sincronizar tabelas:', error);
-});
- */
